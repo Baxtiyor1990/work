8db1ab23ace1633dd0bc69b76de5290da2ea4ab1
Baxtiyor1990 <156651092+Baxtiyor1990@users.noreply.github.com>
2024-03-13 12:57:50 +0500

    описание

--- src/main.py
+++ src/main.py
@@ -15,7 +15,18 @@ def user_interaction():
     print("Ответ API:", hh_vacancies)  # Добавим вывод для отладки
 
     if hh_vacancies and 'items' in hh_vacancies:
-        vacancies_list = [Vacancy(vacancy['name'], vacancy['alternate_url'], vacancy.get('salary', {}).get('from', 'Зарплата не указана'), vacancy.get('description', 'Описание отсутствует')) for vacancy in hh_vacancies['items']]
+        vacancies_list = []
+        for vacancy in hh_vacancies['items']:
+            name = vacancy.get('name', 'Не указано')
+            alternate_url = vacancy.get('alternate_url', 'Не указано')
+
+            # Обработка зарплаты
+            salary_info = vacancy.get('salary')
+            salary_from = salary_info.get('from') if salary_info else 'Зарплата не указана'
+
+            description = vacancy.get('description', 'Описание отсутствует')
+            vacancies_list.append(Vacancy(name, alternate_url, salary_from, description))
+
         filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
         ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
         sorted_vacancies = sort_vacancies(ranged_vacancies)
@@ -24,6 +35,6 @@ def user_interaction():
     else:
         print("Не удалось получить вакансии. Пожалуйста, проверьте запрос и попробуйте снова.")
 
-
 if __name__ == "__main__":
     user_interaction()
+
