414557286dab359d44c5ca06a4c841d13db6025e
Baxtiyor1990 <156651092+Baxtiyor1990@users.noreply.github.com>
2024-03-14 10:49:06 +0500

    ок

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-from api.hh_api import HeadHunterAPI
-from models.vacancy import Vacancy, JSONSaver
-from utils.helpers import filter_vacancies, get_vacancies_by_salary, sort_vacancies, get_top_vacancies, print_vacancies
+from src.api.hh_api import HeadHunterAPI
+from src.models.vacancy import Vacancy
+from src.utils.helpers import print_vacancies
+from src.utils.helpers import filter_vacancies, get_vacancies_by_salary, sort_vacancies, get_top_vacancies
 
 def user_interaction():
     platforms = ["HeadHunter"]
@@ -12,20 +13,23 @@ def user_interaction():
     hh_api = HeadHunterAPI()
     hh_vacancies = hh_api.get_vacancies(search_query)
 
-    print("Ответ API:", hh_vacancies)  # Добавим вывод для отладки
+    print("Ответ API:", hh_vacancies)
 
     if hh_vacancies and 'items' in hh_vacancies:
         vacancies_list = []
-        for vacancy in hh_vacancies['items']:
-            name = vacancy.get('name', 'Не указано')
-            alternate_url = vacancy.get('alternate_url', 'Не указано')
-
-            # Обработка зарплаты
-            salary_info = vacancy.get('salary')
-            salary_from = salary_info.get('from') if salary_info else 'Зарплата не указана'
-
-            description = vacancy.get('description', 'Описание отсутствует')
-            vacancies_list.append(Vacancy(name, alternate_url, salary_from, description))
+        for vacancy_info in hh_vacancies['items']:
+            if isinstance(vacancy_info, dict):
+                name = vacancy_info.get('name', 'Не указано')
+                alternate_url = vacancy_info.get('alternate_url', 'Не указано')
+                salary_info = vacancy_info.get('salary')
+                if salary_info:
+                    salary_from = salary_info.get('from', 'Зарплата не указана')
+                else:
+                    salary_from = 'Зарплата не указана'
+                description = vacancy_info.get('description', 'Описание отсутствует')
+                vacancies_list.append({'name': name, 'alternate_url': alternate_url, 'salary_from': salary_from, 'description': description})
+            elif isinstance(vacancy_info, Vacancy):
+                vacancies_list.append(vacancy_info)
 
         filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
         ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
@@ -37,4 +41,4 @@ def user_interaction():
 
 if __name__ == "__main__":
     user_interaction()
-
+    print("Программа завершила выполнение.")
